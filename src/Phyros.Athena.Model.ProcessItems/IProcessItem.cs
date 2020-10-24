682cd2b714cc0ca8ddb9ec68c070c923a629db0c
toadicusrex <todd.bennion@gmail.com>
2020-10-24 16:43:52 -0600

    Initial commit
    Working on appveyor pipeline

---
+++ src/Phyros.Athena.Model.ProcessItems/IProcessItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Phyros.Athena.Model.ProcessItems
+{
+	public interface IProcessItem
+	{
+		string ProcessItemId { get; }
+		IReadOnlyDictionary<string, object> Data { get; }
+	}
+}
